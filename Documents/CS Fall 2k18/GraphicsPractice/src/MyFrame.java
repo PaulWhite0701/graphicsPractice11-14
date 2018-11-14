06636b9313f79fa309e1b142ecded3d5886854ee
Paul <paul.a.white-1@ou.edu>
2018-11-14 14:38:17 -0600

    Added rectangle demo, and more line stuff.

--- Documents/CS Fall 2k18/GraphicsPractice/src/MyFrame.java
+++ Documents/CS Fall 2k18/GraphicsPractice/src/MyFrame.java
@@ -4,7 +4,7 @@ public class MyFrame extends JFrame
 {
     private final int WIDTH = 300;
     private final int HEIGHT = 400;
-    private MyPanel panel;
+    private static MyPanel panel;
     
     public MyFrame(String title)
     {
@@ -13,11 +13,30 @@ public class MyFrame extends JFrame
         setLocationRelativeTo(null);
         
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+    
+    public void paintLine()
+    {
+        panel = new LineDemoPanel();
+        add(panel);
+        setVisible(true);
+    }
+    
+    public void paintRectangle()
+    {
+        panel = new RectangleDemoPanel();
+        add(panel);
+        setVisible(true);
     }
     
     public static void main (String[] args)
     {
-        MyFrame frame = new MyFrame("Hello");
+        MyFrame frame = new MyFrame("Line");
+        frame.paintLine();
+        
+        MyFrame frame1 = new MyFrame("Rectangle");
+        frame1.paintRectangle();
     }
 
 }
